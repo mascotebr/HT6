e5450aa9cc1aec47c8afe43ea6ce748379fda570
gabriel.conti <gabriel.conti@advisory.com>
2023-12-12 22:41:10 -0300

    Fix

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -6,13 +6,13 @@ export function Countdown() {
     <div>
       <h2 className="title">Vendas se encerram em:</h2>
       <div className="countdown">
-        <span className="title" suppressHydrationWarning>{`${days} dias e ${hours} : ${minutes} : ${seconds} `}</span>
+        <span className="title text-black" suppressHydrationWarning>{`${days} dias e ${hours} : ${minutes} : ${seconds} `}</span>
       </div>
       <div className="ht6-hr" />
       <div style={{ height: 16 }} />
       <h2 className="title">Inicio:</h2>
       <div className="countdown">
-        <span className="title">15 de Janeiro de 2024</span>
+        <span className="title text-black">15 de Janeiro de 2024</span>
       </div>
       <div className="ht6-hr" />
     </div>
--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export function CTA() {
   return (
-    <div className="p-2" style={{backgroundColor: 'var(--secondary-dark)', height: 121}}>
+    <div className="p-2" style={{backgroundColor: 'var(--primary-dark)', height: 121}}>
     <center>
         <p className="text-white p-0 m-2">Venha fazer parte</p>
         <Link href={"https://pay.kiwify.com.br/BFKJu73"}>Garanta sua vaga agora!</Link>
