a6f5240e64edf39e5741cd04c82b555d0f5781ef
gabriel.conti <gabriel.conti@advisory.com>
2023-12-12 22:11:50 -0300

    Final Version v2

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -3,7 +3,7 @@ export function CTA() {
   return (
     <div className="p-2" style={{backgroundColor: 'var(--secondary-dark)', height: 121}}>
     <center>
-        <p className="text-white p-0 m-2">Eae Animou? Não perca mais tempo</p>
+        <p className="text-white p-0 m-2">Venha fazer parte</p>
         <Link href={"https://pay.kiwify.com.br/BFKJu73"}>Garanta sua vaga agora!</Link>
       </center>
     </div>
